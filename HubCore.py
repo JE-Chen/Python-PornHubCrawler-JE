ad0bcaa6a7b71a89fb2b575cfe211d3cc9951e51
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-06-02 19:49:42 +0800

    Add files via upload

--- HubCore.py
+++ HubCore.py
@@ -1,3 +1,5 @@
+import datetime
+import sys
 from HubVideo import HubVideo
 from HubStar import HubStar
 from HubCategories import HubCategories
@@ -6,7 +8,12 @@ from Hub_Photo_And_Gifs import Hub_Photo_And_Gifs
 class HubCore():
 
     def __init__(self):
-        self.HubStar=HubStar()
-        self.HubVideo=HubVideo()
-        self.HubCategories=HubCategories()
-        self.Hub_Photo_And_Gifs=Hub_Photo_And_Gifs()
+        try:
+            self.HubStar=HubStar()
+            self.HubVideo=HubVideo()
+            self.HubCategories=HubCategories()
+            self.Hub_Photo_And_Gifs=Hub_Photo_And_Gifs()
+        except Exception as Errr:
+            print(Errr)
+            sys.exit()
+        print(datetime.datetime.now(),'HubCore Ready',sep=' ')
